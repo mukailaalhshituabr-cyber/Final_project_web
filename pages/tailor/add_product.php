dbe4d0bbd46ae81034128cb70194d99e496b4b7c
Abdoul-Raouf <mukailaalhshituabr@gmail.com>
2025-12-24 16:56:54 +0000

    done

---
+++ pages/tailor/add_product.php
@@ -0,0 +1,15 @@
+pages/customer/profile.php (make sure user can put any kind of picture to his profile and change if he doesn't longer want it)
+pages/tailor/products.php
+pages/tailor/orders.php
+pages/tailor/messages.php
+pages/tailor/add-product.php
+pages/tailor/reviews.php
+pages/tailor/earnings.php
+
+
+
+pages/customer/profile.php (make sure user can put any kind of picture to his profile and change if he doesn't longer want it)
+pages/customer/orders.php
+pages/customer/messages-product.php
+pages/customer/wishlist.php
+pages/customer/fix_folder.php
